6eae971e8ac23b5e0e13805541add22ebe8d8de4
Nabin Khati <nabeenkhati7@gmail.com>
2026-05-13 09:51:54 +0530

    initial commit

---
+++ src/main/java/com/prog10/prog10/App.java
@@ -0,0 +1,13 @@
+package com.prog10.prog10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
